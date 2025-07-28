7f8fe0e3074c42f94a72bc92fb7322ca71b99bdf
Trewasasha <vazheninsaha@gmail.com>
2025-07-28 16:01:18 +0600

    admin

--- models/file.py
+++ models/file.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, BigInteger
 from sqlalchemy.orm import relationship
 from .base import Base
 from datetime import datetime
@@ -9,6 +9,9 @@ class FileModel(Base):
     id = Column(Integer, primary_key=True, index=True)
     filename = Column(String, index=True)  
     path = Column(String)                  
+    size = Column(BigInteger) 
+    modified = Column(DateTime) 
+    hash = Column(String)  # MD5 хэш файла
     owner_id = Column(Integer, ForeignKey("users.id"))
     created_at = Column(DateTime, default=datetime.utcnow)
     
